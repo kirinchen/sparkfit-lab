6dc5642a896ebab8433709d49714e53429cc3fc6
kirin <kirin.chen1001@gmail.com>
2025-08-13 12:49:09 +0800

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sparkfit-lab/',
   plugins: [react()],
   optimizeDeps: {
     include: ['animejs']
