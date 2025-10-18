d8135783914610129d58733cc2e964149b81aa01
kirin <kirin.chen1001@gmail.com>
2025-10-18 09:01:41 +0800

    DONE the new struct

--- src/components/ActivityItemView.tsx
+++ src/components/ActivityItemView.tsx
@@ -13,46 +13,8 @@ const ActivityItemView: React.FC<ActivityItemViewProps> = ({
   onExerciseClick,
   showClickable = true
 }) => {
-  const renderDisplayContent = () => {
-    if (exercise.displayType === 'YouTube' && exercise.displayLink) {
-      // Extract video ID from YouTube URL
-      const videoId = extractYouTubeVideoId(exercise.displayLink);
-      if (videoId) {
-        return (
-          <div className="mb-3">
-            <iframe
-              width="100%"
-              height="200"
-              src={`https://www.youtube.com/embed/${videoId}`}
-              title={exercise.name}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              style={{ borderRadius: '8px' }}
-            ></iframe>
-          </div>
-        );
-      }
-    } else if (exercise.displayImg) {
-      return (
-        <div className="mb-3">
-          <img
-            src={exercise.displayImg}
-            alt={exercise.name}
-            className="img-fluid rounded"
-            style={{ maxHeight: '200px', objectFit: 'cover', width: '100%' }}
-          />
-        </div>
-      );
-    }
-    return null;
-  };
+  
 
-  const extractYouTubeVideoId = (url: string): string | null => {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-    const match = url.match(regExp);
-    return (match && match[2].length === 11) ? match[2] : null;
-  };
 
   return (
     <Card 
@@ -62,7 +24,7 @@ const ActivityItemView: React.FC<ActivityItemViewProps> = ({
     >
       <Card.Body className="d-flex flex-column">
         <Card.Title className="text-cyan-custom">{exercise.name}</Card.Title>
-        {renderDisplayContent()}
+        {/* {renderDisplayContent()} */}
         <Card.Text className="flex-grow-1">
           {exercise.description}
         </Card.Text>
